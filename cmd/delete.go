8d950e843036df48ebf1ec348dc5131dbcceea36
Abraham Cruz <abrahamcz_@hotmail.com>
2025-02-13 15:03:16 -0600

    add: delete command to delete contacts by ID

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,40 +1,38 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
 
+	"github.com/abrahamcruzc/contact-manager-cli/database"
+	"github.com/abrahamcruzc/contact-manager-cli/models"
 	"github.com/spf13/cobra"
 )
+
+var id int 
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Delete contact by id",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		if id == 0 {
+			cmd.Println("Error: invalid ID")
+			return
+		}
+		
+		result := database.DB.Delete(&models.Contact{}, id)
+		
+		if result.RowsAffected == 0 {
+			cmd.Println("Error: No contact was found with that ID")
+		} else {
+			cmd.Println("Contact deleted successfully")
+		}
 	},
 }
 
 func init() {
+	deleteCmd.Flags().IntVarP(&id, "id", "i", 0, "Contact ID")
 	rootCmd.AddCommand(deleteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
